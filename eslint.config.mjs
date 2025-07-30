e61bd58bdfb15d0f2123fe3cb89db9445a28b77d
Andreas Pfau <andreas.pfau@sap.com>
2025-07-30 17:04:59 +0200

    chore(core): delete old dist folder

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,41 +1,13 @@
-import globals from "globals"
 import pluginJs from "@eslint/js"
-import pluginReact from "eslint-plugin-react"
-import jest from "eslint-plugin-jest"
+import typescript from "@typescript-eslint/eslint-plugin"
 
 export default [
-  { files: ["**/*.{js,mjs,cjs,jsx}"] },
   {
-    settings: {
-      react: {
-        version: "detect",
-      },
+    files: ["**/*.{ts,tsx}"],
+    plugins: {
+      "@typescript-eslint": typescript,
     },
-    languageOptions: {
-      globals: {
-        ...globals.browser,
-        ...globals.node,
-      },
-      parserOptions: { ecmaFeatures: { jsx: true } },
-    },
-  },
-  pluginJs.configs.recommended,
-  pluginReact.configs.flat.recommended,
-  // ######### JEST ##########
-  // {
-  //   files: ["**/*.test.js", "**/*.config.js", "__tests__/**", "test/__mocks__/**"],
-  //   languageOptions: { sourceType: "commonjs" },
-  // },
-  {
-    files: ["__tests__/**", "**/*.test.js", "__mocks__/**"],
-    ...jest.configs["flat/recommended"],
-    rules: {
-      ...jest.configs["flat/recommended"].rules,
-      "jest/prefer-expect-assertions": "off",
-      "jest/no-disabled-tests": "off",
-    },
-  },
-  {
-    ignores: ["**/dist/*", "demo/build/*", "coverage/*"],
+
+    ignores: ["**/dist/*"],
   },
 ]
